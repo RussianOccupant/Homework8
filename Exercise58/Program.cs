d180d52ed83ae83c9068e5a183c59507a130c571
AlexBystrov <sshbystrov@gmail.com>
2023-02-08 20:46:19 +0400

    Added answer for exercise 62

---
+++ Exercise58/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+Задача 62: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0)
+                Console.Write($" {array[i, j]} ");
+
+            else Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Main()
+{
+    Console.Clear();
+    int[,] array = new int[4, 4];
+    {
+        int temp = 1;
+        int i = 0;
+        int j = 0;
+
+        while (temp <= array.GetLength(0) * array.GetLength(1))
+        {
+            array[i, j] = temp;
+            temp++;
+            if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+                j++;
+            else if (i < j && i + j >= array.GetLength(0) - 1)
+                i++;
+            else if (i >= j && i + j > array.GetLength(1) - 1)
+                j--;
+            else
+                i--;
+        }
+    }
+    GetArray(array);
+}
+
+Main();
+
+
+
